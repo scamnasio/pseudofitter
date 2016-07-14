a107b28c6faa5d670b1ab96e55d59671a0fd374b
scamnasio <sara.camnasio@gmail.com>
2016-07-14 14:08:32 -0400

    finalized all code

--- allfits.py
+++ allfits.py
@@ -5,10 +5,13 @@ Code to plot up MC fits + all the results of one object
 import numpy as np
 from matplotlib import pyplot as plt
 from numpy import *
+import matplotlib.lines as mlines
 
 def show(path, data_value):
 	'''
 	*path* - last two branches of path to output array file of object
+
+	*data_value* - lmin, lmax, or IP
 	'''
 
 	source = np.genfromtxt("/Users/saracamnasio/Research/Projects/UnusuallyRB/2016_Analysis/New_fits_June16/Gwiazda_fits/{0}".format(path), delimiter=' ', dtype = float)
@@ -73,3 +76,44 @@ def show(path, data_value):
 			p = np.poly1d(coeffs) 
 			plt.plot(W, p(W), color='red', alpha=0.3)
 			plt.scatter(IP, p(IP), color='purple', alpha = 0.3)	
+
+def show_all(path):
+	'''
+	*path* - last two branches of path to output array file of object
+
+	*data_value* - lmin, lmax, or IP
+	'''
+
+	source = np.genfromtxt("/Users/saracamnasio/Research/Projects/UnusuallyRB/2016_Analysis/New_fits_June16/Gwiazda_fits/{0}".format(path), delimiter=' ', dtype = float)
+	DATA_LENGTH = source[:,0]
+	W = np.arange(.8, 1.5, 0.00001)
+	lmin = source[:,5]
+	lmax = source[:,6]
+	IP = source[:,7]
+
+	data1 = mlines.Line2D([], [], color='orange', marker='o', label="Local minima", linestyle='', markersize=5, alpha=0.8)
+	data2 = mlines.Line2D([], [], color='red', marker='o', label="Local maxima", linestyle='', markersize=5, alpha=0.8)
+	data3 = mlines.Line2D([], [], color='purple', marker='o', label="Inflection points", linestyle='', markersize=5, alpha=0.8)
+	
+	plt.figure()
+	plt.ylim(0.85,1.15)
+	plt.xlim(1.15,1.325)
+	plt.legend((data1, data2, data3), ("Local Minima", "Local maxima", "Inflection points"), fontsize= 13 ,loc=1, numpoints=1)
+	# plt.annotate()
+
+	for n in range(len(DATA_LENGTH)):
+		coeff0 = source[n,0]
+		coeff1 = source[n,1]
+		coeff2 = source[n,2]
+		coeff3 = source[n,3]
+		coeff4 = source[n,4]
+		lmin = source[n,5]
+		lmax = source[n,6]
+		IP = source[n,7]
+		coeffs = [coeff4, coeff3, coeff2, coeff1, coeff0]
+		p = np.poly1d(coeffs) 
+		plt.plot(W, p(W), color='grey', alpha=0.1, zorder=-1)
+		plt.scatter(lmin, p(lmin), color='orange', alpha = 0.3, zorder=1)
+		plt.scatter(lmax, p(lmax), color='red', alpha = 0.3, zorder=1)
+		plt.scatter(IP, p(IP), color='purple', alpha = 0.3, zorder=1)
+	
--- pseudo_fitter.py
+++ pseudo_fitter.py
@@ -67,13 +67,14 @@ def MC(N, z):
 		else:
 			pass
 	
-	
+		# If angstroms:
+		# W1 = W1/10000
 		# Trimming the data noisy ends:
 		W2 = W1[W1>1.15]
 		W3 = W2[W2<1.325]
 
 		F2 = F1[W1>1.15]
-		F3 = F2[W2<1.325]
+		F3 = F2[W2<1.325] 
 
 		U2 = U1[W1>1.15]
 		U3 = U2[W2<1.325]
@@ -82,6 +83,7 @@ def MC(N, z):
 		W3[np.isnan(W3)] = 0
 		F3[np.isnan(F3)] = 0
 		U3[np.isnan(U3)] = 0
+		
 	
 		# Recombining the W,F into one array in order to normalize it:
 		recombined = np.vstack([W3,F3,U3])
@@ -102,7 +104,7 @@ def MC(N, z):
 		F = np.array(F)
 		U = np.squeeze(U4)
 		U = np.array(U)
-	
+
 		# Check what the spectrum looks like:
 		plt.figure()
 		plt.errorbar(W, F, yerr=U, color='black') 
@@ -165,27 +167,25 @@ def MC(N, z):
 			second_der = np.array(second_der)
 			minmax_raw1 = p2.r
 			inf_raw1 = p3.r
-			# print "inf_raw is {0} and the min max are: {1}".format(inf_raw, minmax_raw)
-			inf_raw2 = inf_raw1[inf_raw1>1.10]
+			# print "inf_raw is {0}".format(inf_raw)
+			
+			inf_raw2 = inf_raw1[inf_raw1>1.1]
 			inf_raw = inf_raw2[inf_raw2<1.33]
-			if max(minmax_raw1)>1.33:
-				print "Value is greater than boundaries: {0}".format(max(minmax_raw1))
-			elif min(minmax_raw1)<1.10:
-				print "Value is lower than boundaries: {0}".format(min(minmax_raw1))
-			else: 
-				pass
-			minmax_raw2 = minmax_raw1[minmax_raw1>1.10]
+			
+			minmax_raw2 = minmax_raw1[minmax_raw1>1.1]
 			minmax_raw = minmax_raw2[minmax_raw2<1.33]
 
 			if len(inf_raw)>=2:
 				# print "More than one inflection points: {0}. They are {1}".format(len(inf_raw), inf_raw)
+				inf_raw = np.flipud(inf_raw)
 				IP_interest = inf_raw[0]
 				IP.append(IP_interest)
-			elif len(inf_raw)<=1:
+			elif len(inf_raw)==1:
 				# print "Just one inflection point: {0}".format(inf_raw)
 				IP.append(inf_raw[0])
 			else:
-				pass
+				IP.append(0)
+				print "Warning: 0 values for IP"
 
 
 			# Differentiating between loc max and loc min through 2nd deriv test:
@@ -211,6 +211,7 @@ def MC(N, z):
 				lmin.append(lmin_raw[0])
 			else: 
 				print "Warning: 0 values for lmin"
+				lmin.append(0)
 
 			if len(lmax_raw)>=2:
 				lmax_raw = np.flipud(lmax_raw)
@@ -221,6 +222,7 @@ def MC(N, z):
 				# print "Just one local minimum point: {0}".format(lmax_raw)
 				lmax.append(lmax_raw[0])
 			else:
+				lmax.append(0)
 				print "Warning: 0 values for lmax"
 				
 			# Keeping track of the varying spectrum due to MC loop:
@@ -230,9 +232,6 @@ def MC(N, z):
 			# plt.scatter(lmax, p(lmax), color = 'orange', alpha = 1)
 			# plt.scatter(IP, p(IP), color = 'blue', alpha = 1)
 			
-
-		print "the length of the arrays: {0},{1},{2}".format(len(lmax), len(lmin), len(IP))
-
 		# This provides the black base spectrum to be overplotted with the red fits in the line above, leave here!
 		# plt.plot(W, F, color='black')
 		# plt.scatter(lmin, p(lmin), color = 'green', alpha = 1)
@@ -259,23 +258,6 @@ def MC(N, z):
 		mu7,sigma7 = norm.fit(lmin)
 
 		# Trimming out any statistical  outliers:
-
-		# value_1 = [lmin_raw, mu7, sigma7, lmin]
-		# value_2 = [lmax_raw, mu6, sigma6, lmax]
-		# value_3 = [IP_raw, mu5, sigma5, IP]
-
-		''' 
-		legend for this part of the code:
-		v = array of lmin, lmax or IP
-		j = mean value of array of lmin, lmax or IP
-		k = sigma of array of lmin, lmax or IP
-		d = [0,1000] index
-		h = individual value of lmin, lmax, or IP
-
-
-		'''
-		at.random_subset(lmin,950)
-# 
 
 
 		# This plots the histogram distribution of the data (much like the corner plot). It's a sanity check to see if std and mu make sense.
@@ -379,3 +361,193 @@ def MC(N, z):
 		with open("/Users/saracamnasio/Research/Projects/UnusuallyRB/2016_Analysis/New_fits_June16/Results.csv", "a") as fp:
  			wr = csv.writer(fp, dialect='excel')
  			wr.writerow(results_row)
+
+# #value_from_array():
+#	this calculates correct inf points from coeff arrays in output files
+#	not ready
+#	# for i in range(z):
+#		# lmax_raw = [] 
+#		# inf_raw = []
+#		# lmin_raw = []
+#		# medres2.data = medres.data + np.random.randn(medres.data.size)*medres.error
+#		# medres2.baseline(xmin=1.15, xmax=1.325, ymin=0.15, ymax=1.4, subtract=False, highlight_fitregion=False, selectregion=True, exclude=[1.167129, 1.1817135, 1.238683, 1.257725], order=4)
+#		# 
+#		Grabbing the coefficients of the polynomial:
+#		# coeffs = medres2.baseline.baselinepars
+
+#		Storing the coefficient arrays
+#		# coeff0.append(coeffs[4])
+#		# coeff1.append(coeffs[3])
+#		# coeff2.append(coeffs[2])
+#		# coeff3.append(coeffs[1])
+#		# coeff4.append(coeffs[0])
+
+#		Calculating critical points:
+#		# p = np.poly1d(coeffs) 
+#		# p2 = np.polyder(p, m=1)
+#		# first_der = p2(W)
+#		# first_der = np.array(first_der)
+#		# p3 = np.polyder(p, m=2)
+#		# second_der = p3(W)
+#		# second_der = np.array(second_der)
+#		# minmax_raw1 = p2.r
+#		# inf_raw1 = p3.r
+#		print "inf_raw is {0} and the min max are: {1}".format(inf_raw, minmax_raw)
+#		# inf_raw2 = inf_raw1[inf_raw1>1.1]
+#		# inf_raw = inf_raw2[inf_raw2<1.33]
+#		if max(minmax_raw1)>1.33:
+#			print "Value is greater than boundaries: {0}".format(max(minmax_raw1))
+#		elif min(minmax_raw1)<1.10:
+#			print "Value is lower than boundaries: {0}".format(min(minmax_raw1))
+#		else: 
+#			pass
+#		# minmax_raw2 = minmax_raw1[minmax_raw1>1.1]
+#		# minmax_raw = minmax_raw2[minmax_raw2<1.33]
+
+#		# if len(inf_raw)>=2:
+#			print "More than one inflection points: {0}. They are {1}".format(len(inf_raw), inf_raw)
+#			# inf_raw = np.flipud(inf_raw)
+#			# IP_interest = inf_raw[0]
+#			# IP.append(IP_interest)
+#		# elif len(inf_raw)<=1:
+#			print "Just one inflection point: {0}".format(inf_raw)
+#			# IP.append(inf_raw[0])
+#		# else:
+#			# pass
+
+
+#		Differentiating between loc max and loc min through 2nd deriv test:
+#		# 
+#		# for l in range(len(minmax_raw)):
+#			# x = minmax_raw[l]
+#			# y = p3(x)
+#			# if y > 0:
+#				# lmin_raw.append(x)
+#			# elif y < 0:
+#				# lmax_raw.append(x)
+#			# else:
+#				# pass
+
+#		# if len(lmin_raw)>=2:
+#			# lmin_raw = np.flipud(lmin_raw)
+#			print "More than one local minima: {0}. They are {1}".format(len(lmin_raw), lmin_raw)
+#			# lmin_interest = lmin_raw[0]
+#			# lmin.append(lmin_interest)
+#			print "lowest minima is {0}".format(lmin_raw[0])
+#		# elif len(lmin_raw)==1:
+#			print "Just one local minimum point: {0}".format(lmin_raw)
+#			# lmin.append(lmin_raw[0])
+#		# else: 
+#			# print "Warning: 0 values for lmin"
+
+#		# if len(lmax_raw)>=2:
+#			# lmax_raw = np.flipud(lmax_raw)
+#			print "More than one local minima: {0}. They are {1}".format(len(lmax_raw), lmax_raw)
+#			# lmax_interest = lmax_raw[0]
+#			# lmax.append(lmax_interest)
+#		# elif len(lmax_raw)==1:
+#			print "Just one local minimum point: {0}".format(lmax_raw)
+#			# lmax.append(lmax_raw[0])
+#		# else:
+#			# print "Warning: 0 values for lmax"
+#			# 
+#		Keeping track of the varying spectrum due to MC loop:
+#		new_flux = medres2.baseline.basespec
+#		plt.plot(W, new_flux, color='red', alpha=0.1)
+#		plt.scatter(lmin, p(lmin), color = 'green', alpha = 1)
+#		plt.scatter(lmax, p(lmax), color = 'orange', alpha = 1)
+#		plt.scatter(IP, p(IP), color = 'blue', alpha = 1)
+#		# 
+
+#	# print "the length of the arrays: {0},{1},{2}".format(len(lmax), len(lmin), len(IP))
+
+#	This provides the black base spectrum to be overplotted with the red fits in the line above, leave here!
+#	plt.plot(W, F, color='black')
+#	plt.scatter(lmin, p(lmin), color = 'green', alpha = 1)
+#	plt.scatter(lmax, p(lmax), color = 'orange', alpha = 1)
+#	plt.scatter(IP, p(IP), color = 'blue', alpha = 1)
+#	# 
+#	Keeping it real:
+
+#	# lmin = (np.real(lmin))
+#	# lmax = (np.real(lmax))
+#	# IP = (np.real(IP))
+
+#	# print "the length of the arrays: {0},{1},{2}".format(len(lmax), len(lmin), len(IP))
+# #
+
+#	This calculates mean and standard deviation for all of the coefficients & critical points
+#	# mu0,sigma0 = norm.fit(coeff0)
+#	# mu1,sigma1 = norm.fit(coeff1)
+#	# mu2,sigma2 = norm.fit(coeff2)
+#	# mu3,sigma3 = norm.fit(coeff3)
+#	# mu4,sigma4 = norm.fit(coeff4)
+#	# mu5,sigma5 = norm.fit(IP)
+#	# mu6,sigma6 = norm.fit(lmax)
+#	# mu7,sigma7 = norm.fit(lmin)
+
+#	Trimming out any statistical  outliers:
+
+
+
+
+#	# 
+#	# plt.figure()
+#	# plt.hist(IP, 10, normed=True, facecolor='orange', histtype='stepfilled')
+#	# plt.title("Inflection Point")
+#	# plt.ylabel('Probability')
+#	# plt.xlabel('IP value')
+#	# plt.savefig('/Users/saracamnasio/Research/Projects/UnusuallyRB/2016_Analysis/New_fits_June16/{0}_{1}/{1}_hist_IP.png'.format(n, name), format='png')
+#	plt.close()
+
+#	# plt.figure()
+#	# plt.hist(lmax, 10, normed=True, facecolor='orange', histtype='stepfilled')
+#	# plt.title("Local Maximum")
+#	# plt.ylabel('Probability')
+#	# plt.xlabel('Lmax value')
+#	# plt.savefig('/Users/saracamnasio/Research/Projects/UnusuallyRB/2016_Analysis/New_fits_June16/{0}_{1}/{1}_hist_lmax.png'.format(n, name), format='png')
+#	plt.close()
+
+#	# plt.figure()
+#	# plt.hist(lmin, 10, normed=True, facecolor='orange', histtype='stepfilled')
+#	# plt.title("Local Minimum")
+#	# plt.ylabel('Probability')
+#	# plt.xlabel('Lmin value')
+#	# plt.savefig('/Users/saracamnasio/Research/Projects/UnusuallyRB/2016_Analysis/New_fits_June16/{0}_{1}/{1}_hist_lmin.png'.format(n, name), format='png')
+#	plt.close()
+
+#	# coeff0 = np.array(coeff0)
+#	# coeff1 = np.array(coeff1)
+#	# coeff2 = np.array(coeff2)
+#	# coeff3 = np.array(coeff3)
+#	# coeff4 = np.array(coeff4)
+#	# IP = np.array(IP)
+#	# lmin = np.array(lmin)
+#	# lmax = np.array(lmax)
+
+#	Making corner plot:
+#	coeff_MC = np.vstack([coeff0, coeff1, coeff2, coeff3, coeff4])
+#	coeff_MC2 =  np.transpose(coeff_MC)
+# #
+#	figure = corner.corner(coeff_MC2, labels=[r"$0th Coefficient$", r"$1st Coefficient$", r"$2nd Coefficient$", r"$3rd Coefficient$", r"$4th Coefficient$"], quantiles=[0.16, 0.5, 0.84], plot_contours=True, label_args={'fontsize':15}, color='black')
+#	figure.gca().annotate("MC Uncertainty Analysis of {0} Pyspeckit Fitting".format(name), xy=(1.2, 1.0), xycoords="figure fraction", xytext=(0, -5), textcoords="offset points", ha="center", va="top")
+#	plt.savefig('/Users/saracamnasio/Research/Projects/UnusuallyRB/2016_Analysis/New_fits_June16/{0}_{1}/{1}_MCplot_coeffs.png'.format(n, name), format='png')
+#	plt.close()
+# #
+#	CP_MC = np.vstack([IP, lmin, lmax])
+#	CP_MC2 =  np.transpose(CP_MC)
+
+#	figure2 = corner.corner(CP_MC2, labels=[r"$Inflection Point$", r"$Local Min$", r"$Local max$"], quantiles=[0.16, 0.5, 0.84], plot_contours=True, label_args={'fontsize':15}, color='black')
+#	figure2.gca().annotate("MC Uncertainty Analysis of {0} Pyspeckit Fitting".format(name), xy=(1.2, 1.0), xycoords="figure fraction", xytext=(0, -5), textcoords="offset points", ha="center", va="top")
+#	plt.savefig('/Users/saracamnasio/Research/Projects/UnusuallyRB/2016_Analysis/New_fits_June16/{0}_{1}/{1}_MC_CPpoints.png'.format(n, name), format='png')
+#	plt.close()
+# #
+#	# output_arrays = np.vstack([coeff0, coeff1, coeff2, coeff3, coeff4, lmin, lmax, IP])
+#	# output_arrays = np.transpose(output_arrays)
+#	# np.savetxt('/Users/saracamnasio/Research/Projects/UnusuallyRB/2016_Analysis/New_fits_June16/{0}_{1}/{1}_unc_arrays.txt'.format(n, name), output_arrays)
+
+#	# results_row = [n, name, mu0, sigma0, mu1, sigma1, mu2, sigma2, mu3, sigma3, mu4, sigma4, mu5, sigma5, mu6, sigma6, mu7, sigma7]
+
+#	# with open("/Users/saracamnasio/Research/Projects/UnusuallyRB/2016_Analysis/New_fits_June16/Results.csv", "a") as fp:
+#		# wr = csv.writer(fp, dialect='excel')
+#		# wr.writerow(results_row)
